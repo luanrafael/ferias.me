5dd8e71accf79c30ead09a09ee118d9a81f1cf4f
Luan Rafael C. Pinheiro <luan-g8@hotmail.com>
2014-05-09 15:55:56 -0300

    Update ferias.js

--- ferias.js
+++ ferias.js
@@ -1,56 +1,58 @@
 var HH = 19;
 
-		var MI = 00;
+var MI = 00;
 
-		var SS = 00;
+var SS = 00;
 
-		var ferias = new Date(2014,4,9,HH,MI,SS);
-		var retorno = new Date(2014,5,1,13,00,00);
-		var isFerias = false;
-
-		function atualizaContador() {
-
-			if(!isFerias){
-				contador(ferias,'Férias chegando em:\n','');
-			} else {
-				contador(retorno,'Estou de FÉRIAS, volto em:\n','As férias acabaram :( ... go go Work now!!!');
-			}
-			setTimeout(atualizaContador,1000);
-		}
-
-		function contador (futuro,strfaltam,strcalback) {
-			var hoje = new Date();
-			var ss = parseInt((futuro - hoje) / 1000);
-			var mm = parseInt(ss / 60);
-			var hh = parseInt(mm / 60);
-			var dd = parseInt(hh / 24);
-			ss = ss - (mm * 60);
-			mm = mm - (hh * 60);
-			hh = hh - (dd * 24);
-			var faltam = strfaltam + ' ';
-			
-			faltam += (dd > 0) ? formatstr(dd) + 'dia(s)\n' : '' ;
-
-			var fhoras = formatstr(hh) + ':' + formatstr(mm) + ':' + formatstr(ss);
-
-			faltam += fhoras;
+var ferias = new Date(2014,4,9,HH,MI,SS);
+var retorno = new Date(2014,5,1,13,00,00);
 
 
-			if (dd+hh+mm+ss > 0) {
-				document.getElementById('contador').innerText = faltam;
-			} else {
-				if(!isFerias){
-					isFerias = true;
-				} else {
-					document.getElementById('contador').innerText = strcalback;
+var isFerias = false;
 
-				}
-			}
-			
+function atualizaContador() {
+
+	if(!isFerias){
+		contador(ferias,'Férias chegando em:\n','');
+	} else {
+		contador(retorno,'Estou de FÉRIAS, volto em:\n','As férias acabaram :( ... go go Work now!!!');
+	}
+	setTimeout(atualizaContador,1000);
+}
+
+function contador (futuro,strfaltam,strcalback) {
+	var hoje = new Date();
+	var ss = parseInt((futuro - hoje) / 1000);
+	var mm = parseInt(ss / 60);
+	var hh = parseInt(mm / 60);
+	var dd = parseInt(hh / 24);
+	ss = ss - (mm * 60);
+	mm = mm - (hh * 60);
+	hh = hh - (dd * 24);
+	var faltam = strfaltam + ' ';
+	
+	faltam += (dd > 0) ? formatstr(dd) + 'dia(s)\n' : '' ;
+
+	var fhoras = formatstr(hh) + ':' + formatstr(mm) + ':' + formatstr(ss);
+
+	faltam += fhoras;
+
+
+	if (dd+hh+mm+ss > 0) {
+		document.getElementById('contador').innerText = faltam;
+	} else {
+		if(!isFerias){
+			isFerias = true;
+		} else {
+			document.getElementById('contador').innerText = strcalback;
 
 		}
+	}
+	
 
-		function formatstr(str){
+}
 
-			return (str.toString().length == 1) ? '0'+ str : str;
-		}
+function formatstr(str){
+
+	return (str.toString().length == 1) ? '0'+ str : str;
+}
